273d042095b6cf97ce97df4c1fcb8349a878e486
Alan Gardin <gardin.alan@gmail.com>
2019-01-16 15:04:00 +0100

    Fixed bugs in AVL trees insert algorithm. New to implement AvlTreeNode to finish

--- data_structures/trees/AvlTree.cpp
+++ data_structures/trees/AvlTree.cpp
@@ -33,7 +33,7 @@ void AvlTree<T>::update_height() {
     else if (this->left != nullptr)
         this->height = this->left->height + 1;
     else if (this->right != nullptr)
-        this->height = max(this->left->height, this->right->height) + 1;
+        this->height = this->right->height + 1;
     else
         this->height = 1;
 }
@@ -42,7 +42,7 @@ void AvlTree<T>::update_height() {
 template <class T>
 bool AvlTree<T>::is_balanced() {
     if (this->left != nullptr && this->right != nullptr)
-        return  (abs(this->left->height - this->right->height) <= 1);
+        return (abs(this->left->height - this->right->height) <= 1);
     else if (this->left != nullptr)
         return (this->left->height <= 1);
     else if (this->right != nullptr) {
@@ -63,10 +63,10 @@ AvlTree<T>* AvlTree<T>::left_rotation() {
     y->left = x;
 
     // Update heights
-    y->height = max(y->left->height, y->right->height) + 1;
-    x->height = max(x->left->height, x->right->height) + 1;
+    x->update_height();
+    y->update_height();
 
-    return x;
+    return y;
 }
 
 
@@ -80,67 +80,71 @@ AvlTree<T>* AvlTree<T>::right_rotation() {
     x->right = y;
 
     // Update heights
-    y->height = max(y->left->height, y->right->height) + 1;
-    x->height = max(x->left->height, x->right->height) + 1;
+    x->update_height();
+    y->update_height();
 
-    return x;
+    return y;
 }
 
 
 template <class T>
 queue<AvlTree<T>*> AvlTree<T>::safe_insert(const T &value) {
-    queue<AvlTree<T>*> children;
+    queue<AvlTree<T>*> root_children;
 
     // Regular binary search tree insert
     if (value < this->data) {
         if (this->left != nullptr) {
-            children = this->left->safe_insert(value);
+            root_children = this->left->safe_insert(value);
 
-            if (children.size() == 0) // A balancing already occurred and has been dealt with already
-                return children;
-            else if (children.size() == 1) { // A balancing already occurred and the queue contains the new root
-                this->left = children.front(); // Update the new root
+            if (root_children.size() == 0) // A balancing already occurred and has been dealt with already
+                return root_children;
+            else if (root_children.size() == 1) { // A balancing already occurred and the queue contains the new root
+                this->left = root_children.front(); // Update the new root
                 this->update_height();
                 return queue<AvlTree<T>*>();
             }
         }
         else {
             this->left = new AvlTree(value);
-            children.push(nullptr);  // Grand child first (will never be used anyway)
-            children.push(this->left);  // Then child
+            root_children.push(this->left);  // Grand child first (will never be used anyway)
+            root_children.push(this);  // Then child
+            this->update_height();
+            return root_children; // Finish here, because this tree is balanced
         }
     }
     else {
         if (this->right != nullptr) {
-            children = this->right->safe_insert(value);
+            root_children = this->right->safe_insert(value);
 
-            if (children.size() == 0) // A balancing already occurred and has been dealt with already
-                return children;
-            else if (children.size() == 1) { // A balancing already occurred and the queue contains the new root
-                this->right = children.front(); // Update the new root
+            if (root_children.size() == 0) // A balancing already occurred and has been dealt with already
+                return root_children;
+            else if (root_children.size() == 1) { // A balancing already occurred and the queue contains the new root
+                this->right = root_children.front(); // Update the new root
                 this->update_height();
                 return queue<AvlTree<T>*>();
             }
         }
         else {
             this->right = new AvlTree(value);
-            children.push(nullptr);  // Grand child first (will never be used anyway)
-            children.push(this->right);  // Then child
+            root_children.push(this->right);  // Grand child first (will never be used anyway)
+            root_children.push(this);  // Then child
+            this->update_height();
+            return root_children; // Finish here, because this tree is balanced
         }
     }
 
 
     // If reached, no balancing has occurred, and we are looking for an unbalanced node
     if (this->is_balanced()) {
-        children.pop();  // Remove grand child. Now the child is is the grand child
-        children.push(this);  // Push new child
+        root_children.pop();  // Remove grand child. Now the child is is the grand child
+        root_children.push(this);  // Push new child
 
-        return children;
+        return root_children;
     }
     else { // Unbalanced node found
         AvlTree<T>* old_root = this;
-        AvlTree<T>* child = children.back();
-        AvlTree<T>* grandchild = children.front();
+        AvlTree<T>* child = root_children.back();
+        AvlTree<T>* grandchild = root_children.front();
 
         AvlTree<T>* new_root = nullptr;
 
@@ -165,8 +169,10 @@ queue<AvlTree<T>*> AvlTree<T>::safe_insert(const T &value) {
         }
 
         // Return the new root of this subtree
-        children.push(new_root);
-        return children;
+        root_children.pop();
+        root_children.pop();
+        root_children.push(new_root);
+        return root_children;
     }
 }
 
@@ -174,11 +180,9 @@ queue<AvlTree<T>*> AvlTree<T>::safe_insert(const T &value) {
 
 int main() {
     AvlTree<int>* tree = new AvlTree<int>(1);
-    tree->insert(0);
     tree->insert(2);
     tree->insert(3);
-    tree->insert(1);
-    tree->insert(4); // FIXME: a rotation is supposed to happen here
+    tree->insert(4);
 
     cout << "Traversal: " << endl;
     tree->traversal_in_order();
